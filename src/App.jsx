3fd2ad55a1589eccd803e37371b3b05942b24f6d
Deepak Singh <105776457+deepak-singh123@users.noreply.github.com>
2025-04-26 17:19:03 +0530

    Update App.jsx register.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Register from './register'
+import Register from './Register'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home'
 import Login from './Login'
