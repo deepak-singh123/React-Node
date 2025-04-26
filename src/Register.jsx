769bde4cc74ac4383e4595d1f3ca44d62f42b829
Deepak Singh <105776457+deepak-singh123@users.noreply.github.com>
2025-04-26 17:23:21 +0530

    Update Register.jsx link of backend

--- src/Register.jsx
+++ src/Register.jsx
@@ -32,7 +32,8 @@ const Register = () => {
       console.log(`${key}: ${value}`);
     }
         try{
-     const response = await fetch('http://localhost:5000/user/register',{
+     const response = await fetch('https://react-node-sqgq.onrender.com/user/register',{
+       
         method:"POST",
         headers: {
           "Content-Type": "application/json", // Sending JSON data
